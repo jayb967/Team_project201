da001bd22fae261eda2e77b80ab2af5b1b2eb918
rgallus1 <roseannegallus@yahoo.com>
2016-12-19 12:30:11 -0800

    added listeners and code to time users response

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 
+'use strict';
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+//DOM variables
+var userName = document.getElementById('user-name');
+
+//Event Listeners for Main Page
+userName.addEventListener('submit', handleEvents);
+wantToPlay.addEventListener('click', handleEvents);
+startGame.addEventListener('click', handleEvents);
+displayScore.addEventListener('click', handleEvents);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+//this code is to be used when we are waiting for the user to click on cards
+//When a visitor clicks the button, the setTimeout() method is called, passing in the expression that
+// we want to run after the time delay, and the value of the time delay itself - 5,000 milliseconds or 5 seconds.
+//<input type="button" name="clickMe" value="Click me and wait!"
+//onclick="setTimeout('alert(\'Surprise!\')', 5000)"/>
+
+localStorage.userName = event.target.userName.value;
