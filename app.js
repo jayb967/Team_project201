14ec8585ab1ca6f0cc8a40d89e5f164e01e6ea5c
radenska <yana_radenska@yahoo.com>
2016-12-22 14:09:44 -0800

    saving changes before merge

--- app.js
+++ app.js
@@ -272,7 +272,7 @@ function newInstructionsButton () {
 
 function seeInitialInstructions() {
   var h3El = document.createElement('h3');
-  h3El.textContent = 'Four rows of four cards placed facing down are displayed. Once the Start Game button is clicked the timer will begin and the user will have the ability to click on only two cards at a time. If a match occurs the cards will remain facing up.  If a match doesn’t occur the cards will automatically be turned face down.  If the player qualifies within the top ten, they may register their score or they may play again. If they choose to register their score, they will be transported to the Top Scores Page. If they choose to Play Again the cards will be turned over, the timer reset and the player can begin clicking on cards.'
+  h3El.textContent = 'The game starts with rows of cards all facing down. Once you click one of the play buttons, an invisible timer will start. You turn cards over by clicking on them, revealing two cards per round. If a match occurs the cards will remain facing up.  If a match doesn’t occur the cards will automatically be turned face down.  Repeat this process until you have matched all the pairs. Good luck!';
   wantToPlay.appendChild(h3El);
   seeInstructions.innerHTML='';
   var hideInstructionsButton = document.createElement('button');
@@ -318,7 +318,7 @@ function playAgainButtons() {
   var tempNewSize;
   if (boardSize === 8) {
     tempSize = '4x4 BOARD AGAIN';
-    tempNewSize = '6x6 INSTEAD'
+    tempNewSize = '6x6 INSTEAD';
   } else {
     tempSize = '6x6 BOARD AGAIN';
     tempNewSize = '4x4 INSTEAD';
